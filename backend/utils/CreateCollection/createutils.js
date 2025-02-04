bcc4c4156d9abca93cdd3461ba6dcdb7ac989fef
paranoid-llama <omar_born@hotmail.com>
2025-02-04 00:25:23 -0500

    export update 1-3
    testing error on prod (i dont know how to reproduce this error)

--- backend/utils/CreateCollection/createutils.js
+++ backend/utils/CreateCollection/createutils.js
@@ -2,8 +2,11 @@ import { genGames } from "../../common/infoconstants/miscconstants.mjs"
 
 function getAPIGenFormat(collectionGen) {
     const gameOrHomeGen = isNaN(parseInt(collectionGen))
+    console.log(collectionGen)
     if (gameOrHomeGen) {
         if (collectionGen === 'home') {return 'home'}
+        console.log(genGames)
+        console.log(genGames.filter(gG => gG.games.includes(collectionGen)))
         const genNum = genGames.filter(gG => gG.games.includes(collectionGen))[0].gen
         return `gen${genNum}`
     }
